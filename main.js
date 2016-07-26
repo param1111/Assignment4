2349af65131c8b2c0d6606ab9f4d435b2e561120
Paramveer Singh <paramveer.singh2@wipro.com>
2016-07-26 11:30:39 +0530

    Changes to the js

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ $(function (){
 	var order = $("#dataTable");
 	var $name = $("#inputName");
 	var $age = $("#inputAge");
+	var $gender = $("#inputGender");
+	var $company = $("#inputCompany");
+	var $email = $("#inputEmail");
+	var $phone = $("#inputPhone");
 	var orderTemplate = $("#data-template").html();
 	function dataOrder(orders){
-		order.append(Mustache.render(orderTemplate,orders));
+		order.append(Mustache.render(orderTemplate,orders));/*Passing Template and Object*/
 	}
 	$.ajax({
 		type: 'GET',
@@ -22,6 +26,10 @@ $(function (){
 		var newOrder = {
 			name: $name.val(),
 			age: $age.val(),
+			gender: $gender.val(),
+			company: $company.val(),
+			email: $email.val(),
+			phone: $phone.val(),
 		};
 		$.ajax({
 			type: 'POST',
@@ -29,22 +37,25 @@ $(function (){
 			data: newOrder,
 			success:function(addOrder){
 				dataOrder(addOrder);
+			},
+			error:function(){
+				alert("Error Posting");
 			}
 		});
 
 	});
-
-
-	order.delegate(".remove" , "click",function(){
-		var $li= $(this).closest('li');
+	order.delegate(".remove" , "click",function(){  /*it will listen to any click events on order things,fires only when its part of remove class*/
+		var $li= $(this).closest('li');				/*Refers to the remove button that is clicked*/
 		
 		$.ajax({
 			type: 'DELETE',
-			url:"http://localhost:8080/employee/" + $(this).attr("data-id"),
+			url:"http://localhost:8080/employee/" + $(this).attr("data-id"),/*Getting/grabbing the attribute out*/
 			success:function(){
 				$li.remove();
-			}
-			
+			},
+			error:function(){
+				alert("Error Deleting Data");
+			}			
 		});
 
 		});
@@ -63,7 +74,11 @@ $(function (){
 		var $li= $(this).closest('li');
 		var order1 = {
 			name:$li.find("input.name").val(),
-			age:$li.find("input.age").val()
+			age:$li.find("input.age").val(),
+			gender:$li.find("input.gender").val(),
+			company:$li.find("input.company").val(),
+			email:$li.find("input.email").val(),
+			phone:$li.find("input.phone").val()
 		};
 		$.ajax({
 			type:'PUT',
@@ -72,6 +87,10 @@ $(function (){
 			success:function(addOrder){
 				$li.find("span.name").html(order1.name);
 				$li.find("span.age").html(order1.age);
+				$li.find("span.gender").html(order1.gender);
+				$li.find("span.company").html(order1.company);
+				$li.find("span.email").html(order1.email);
+				$li.find("span.phone").html(order1.phone);
 				$li.removeClass("edit");
 			},
 			error:function(){
